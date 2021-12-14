0a20aca9c1242f66afe3b5f43d5b1af18b0d7c1a
Anders Olme <anders.olme@gmail.com>
2021-12-14 21:29:27 +0100

    re-learnt basic math..

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,18 @@ const c = @cImport({
 });
 const std = @import("std");
 const math = @import("std").math;
+const rand = @import("std").rand;
+
 const WINDOW_WIDTH: i32 = 1024;
 const WINDOW_HEIGHT: i32 = 768;
+
+const RndGen = rand.DefaultPrng;
 
 const Star = struct {
     x: f32,
     y: f32,
     delta: f32,
+    m: f32,
     r: u8,
     g: u8,
     b: u8,
@@ -19,16 +24,19 @@ const Star = struct {
             .x = x,
             .y = y,
             .delta = delta,
+            .m = y - delta * x,
             .r = 0xff,
             .g = 0xff,
             .b = 0xff,
         };
     }
     pub fn init2(x: f32, y: f32) Star {
+        var delta: f32 = (@intToFloat(f32, WINDOW_HEIGHT / 2) - y) / (@intToFloat(f32, WINDOW_WIDTH / 2) - x);
         return Star{
             .x = x,
             .y = y,
-            .delta = (@intToFloat(f32, WINDOW_WIDTH / 2) - x) / (@intToFloat(f32, WINDOW_HEIGHT / 2) - y),
+            .delta = delta,
+            .m = y - x * delta,
             .r = 0xff,
             .g = 0xff,
             .b = 0xff,
@@ -37,6 +45,7 @@ const Star = struct {
 };
 
 pub fn main() anyerror!void {
+    var rnd = rand.DefaultPrng.init(42);
     std.log.info("A small sdl test.", .{});
 
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -65,6 +74,7 @@ pub fn main() anyerror!void {
     var pos_y: i32 = WINDOW_HEIGHT / 2;
     var star = Star.init2(@intToFloat(f32, WINDOW_WIDTH / 2) + 10.0, @intToFloat(f32, WINDOW_HEIGHT / 2) + 10.0);
     std.debug.print("-> {d}\n", .{star});
+
     while (!quit) {
         _ = c.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
         _ = c.SDL_RenderClear(renderer);
@@ -77,19 +87,22 @@ pub fn main() anyerror!void {
                 else => {},
             }
         }
-        var dots: u8 = 0;
 
-        while (dots < 200) {
-            _ = c.SDL_SetRenderDrawColor(renderer, @truncate(u8, 0xFF +% dots), @truncate(u8, 0xFF + @bitCast(u32, pos_y) + dots), @truncate(u8, 0xFF + @bitCast(u32, pos_x) + dots), 0x00);
+        _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
 
-            pos_y = @floatToInt(i32, 100.0 * math.sin(@intToFloat(f64, pos_x) / 10.0)) + WINDOW_HEIGHT / 2;
-            pos_x += 1;
-            if (pos_x > WINDOW_WIDTH) {
-                pos_x = 0;
-            }
-            _ = c.SDL_RenderDrawPoint(renderer, pos_x, pos_y);
-            dots += 1;
+        if (star.x < WINDOW_WIDTH / 2) {
+            star.x -= 1.5;
+        } else {
+            star.x += 1.5;
         }
+        star.y = star.x * star.delta + star.m;
+        _ = c.SDL_RenderDrawPoint(renderer, @floatToInt(i32, star.x), @floatToInt(i32, star.y));
+        if (star.x < 0 or star.x > WINDOW_WIDTH) {
+            star = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
+        } else if (star.y < 0 or star.y > WINDOW_HEIGHT) {
+            star = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
+        }
+
         //_ = c.SDL_RenderCopy(renderer, zig_texture, null, null);
         c.SDL_RenderPresent(renderer);
 
