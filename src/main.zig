96e8a8e27e6bb18d2e73a2095e9eba458d88f6ac
Anders Olme <anders.olme@gmail.com>
2021-12-14 21:42:18 +0100

    Some progress
    Now we have a starfield but it dont looks that nice.

--- src/main.zig
+++ src/main.zig
@@ -72,8 +72,13 @@ pub fn main() anyerror!void {
     var quit = false;
     var pos_x: i32 = 0;
     var pos_y: i32 = WINDOW_HEIGHT / 2;
-    var star = Star.init2(@intToFloat(f32, WINDOW_WIDTH / 2) + 10.0, @intToFloat(f32, WINDOW_HEIGHT / 2) + 10.0);
-    std.debug.print("-> {d}\n", .{star});
+    //var star = Star.init2(@intToFloat(f32, WINDOW_WIDTH / 2) + 10.0, @intToFloat(f32, WINDOW_HEIGHT / 2) + 10.0);
+    var stars: [500]Star = undefined;
+
+    for (stars) |*st, _| {
+        st.* = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
+    }
+    //std.debug.print("-> {d}\n", .{star});
 
     while (!quit) {
         _ = c.SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
@@ -89,23 +94,23 @@ pub fn main() anyerror!void {
         }
 
         _ = c.SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0x00);
-
-        if (star.x < WINDOW_WIDTH / 2) {
-            star.x -= 1.5;
-        } else {
-            star.x += 1.5;
+        for (stars) |*star| {
+            if (star.x < WINDOW_WIDTH / 2) {
+                star.x -= 1.0;
+            } else {
+                star.x += 1.0;
+            }
+            star.y = star.x * star.delta + star.m;
+            _ = c.SDL_RenderDrawPoint(renderer, @floatToInt(i32, star.x), @floatToInt(i32, star.y));
+            if (star.x < 0 or star.x > WINDOW_WIDTH) {
+                star.* = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
+            } else if (star.y < 0 or star.y > WINDOW_HEIGHT) {
+                star.* = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
+            }
         }
-        star.y = star.x * star.delta + star.m;
-        _ = c.SDL_RenderDrawPoint(renderer, @floatToInt(i32, star.x), @floatToInt(i32, star.y));
-        if (star.x < 0 or star.x > WINDOW_WIDTH) {
-            star = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
-        } else if (star.y < 0 or star.y > WINDOW_HEIGHT) {
-            star = Star.init2(rnd.random.float(f32) * @intToFloat(f32, WINDOW_WIDTH), rnd.random.float(f32) * @intToFloat(f32, WINDOW_HEIGHT));
-        }
-
         //_ = c.SDL_RenderCopy(renderer, zig_texture, null, null);
         c.SDL_RenderPresent(renderer);
 
-        c.SDL_Delay(30);
+        c.SDL_Delay(17);
     }
 }
